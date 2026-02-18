c635c94fa94a4e1c29e0543b1716d54aa7429af4
AdenorGit <adenor@gmail.com>
2026-02-18 14:19:03 -0300

    Remodelagem

--- App/View/Include/inc_admin_footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer mt-auto bg-dark-subtle py-3 border-top">
-    <div class="container text-center">
-        <span>© 2026 <?= NOME_AUTOR ?>. Todos os direitos reservados.</span>
-    </div>
-</footer>
-
-</body>
-
-</html>
--- App/View/Include/inc_msg.php
+++
@@ -1,3 +0,0 @@
-<div class="alert alert-warning d-none " style="margin-top: 4rem;" role="alert">
-    A simple warning alert—check it out!
-</div>
